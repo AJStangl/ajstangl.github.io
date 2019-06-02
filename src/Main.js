297df9f0318895bf06ea85aa08ab1e10a1ebecdf
AJ <ajstangl@gmail.com>
2019-06-02 10:35:15 -0400

    - Gave it some awesome font awesome

--- src/Main.js
+++ src/Main.js
@@ -4,28 +4,35 @@ import {
     NavLink,
     HashRouter
 } from "react-router-dom";
-
 import Home from "./Components/home";
 import Resume from "./Components/resume";
 import Contact from "./Components/contat";
+import { faHome, faEnvelope, faNewspaper } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+const HomeIcon = <FontAwesomeIcon icon={faHome}/>;
+const EmailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+const ResumeIcon = <FontAwesomeIcon icon={faNewspaper} />;
 
 class Main extends Component {
     render() {
         return (
             <HashRouter>
-            <div>
-                <h1>Personal Website</h1>
-                <ul className="header">
-                    <li><NavLink to="/" >Home</NavLink></li>
-                    <li><NavLink to="/resume">Resume</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
-                </ul>
-                <div className="content">
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/resume" component={Resume}/>
-                    <Route path="/contact" component={Contact}/>
+                <div>
+                    <ul className="header">
+                        <li>
+                            <NavLink exact to="/">Home {HomeIcon}</NavLink> />
+                            <NavLink to="/resume">Resume {ResumeIcon}</NavLink>
+                            <NavLink to="/contact">Contact {EmailIcon}</NavLink>
+                        </li>
+                    </ul>
+                    <div className="content">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/resume" component={Resume}/>
+                        <Route path="/contact" component={Contact}/>
+                    </div>
                 </div>
-            </div>
             </HashRouter>
         );
     }
